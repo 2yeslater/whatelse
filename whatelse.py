9e49cb411a104cd4f616f271c7e0a1313af7ec48
2yeslater <dengshize@pigdeMacBook-Air.local>
2014-10-30 18:04:09 +0800

    同ip网站查询
    同ip网站查询，调用站长工具的数据，查询过频繁会导致网站启用验证码验证。
    对了，还要安装BeautifulSoup ，如果安装了easy_install 的话，可以通过命令安装：easy_install
    BeautifulSoup
    当然也可以用其他方式安装。

---
+++ whatelse.py
@@ -0,0 +1,59 @@
+#coding=utf-8
+import urllib
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import sys
+
+#########################################################################
+#
+#	by xingfenxiaozhu
+#	blog www.xingfenxiaozhu.com
+#	description:check if there's any other website at the same webserver
+#	
+#
+#########################################################################
+
+
+data_url="http://s.tool.chinaz.com/same"
+#referer
+referer=data_url
+if len(sys.argv)!=2:
+	print "######################################################"
+	print "##    usage: python whatelse.py www.baidu.com[ip]   ##"
+	print "######################################################"
+	exit()
+
+#the ip or domain you want to search
+#你想搜索的目标ip或者域名
+target_url=sys.argv[1]
+
+post_data={"s":target_url}
+
+encoded_data=urllib.urlencode(post_data)
+
+req=urllib2.Request(data_url,encoded_data)
+
+req.add_header("Referer",referer)
+
+req.add_header("Host",'s.tool.chinaz.com')
+
+print "here we go!waiting....."
+
+response=urllib2.urlopen(req)
+
+content=response.read()
+print "results as follow:"
+try:
+	soup=BeautifulSoup(content)
+
+	content=soup.find(id="contenthtml")
+	if len(content.text)==8:
+		print 'nothing found'
+	else:
+		result=''
+		for i in content.ul:
+			print i.a.text
+except Exception,e:
+
+	print "Ops, Something goes wrong or being limited !"
+	
